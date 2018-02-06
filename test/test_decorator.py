992f00c84d9b7bfdd5faf6fd8fe794989a808584
Alex Sherman <asherman1024@gmail.com>
2018-02-05 22:51:49 -0600

    Either make unsync threadsafe or thread-unsafe

--- test/test_decorator.py
+++ test/test_decorator.py
@@ -7,7 +7,26 @@ class DecoratorTests(TestCase):
     def test_1(self):
         @unsync
         async def faff():
-            await asyncio.sleep(0)
+            await asyncio.sleep(0.01)
             return True
         self.assertTrue(faff().result())
-        pass
+
+    def test_call_blocking(self):
+        calls = []
+
+        @unsync
+        async def other():
+            calls.append('a')
+            await asyncio.sleep(0.01)
+            calls.append('c')
+            return True
+
+        @unsync
+        async def long(task):
+            calls.append('b')
+            await task
+            calls.append('d')
+            return True
+        o = other()
+        long(o).result()
+        self.assertEqual(['a', 'b', 'c', 'd'], calls)
--- unsync/unsync.py
+++ unsync/unsync.py
@@ -1,6 +1,35 @@
 import asyncio
+import concurrent
+from asyncio import compat
 from threading import Thread
 
+
+class Unfuture:
+    def __init__(self, coro, loop):
+        self.future = asyncio.ensure_future(coro, loop=loop)
+        self.loop = loop
+
+        future = concurrent.futures.Future()
+
+        def callback():
+            try:
+                asyncio.futures._chain_future(self.future, future)
+            except Exception as exc:
+                if future.set_running_or_notify_cancel():
+                    future.set_exception(exc)
+                raise
+
+        loop.call_soon_threadsafe(callback)
+        self.con_future = future
+
+    def __iter__(self):
+        yield from self.future.__iter__()
+
+    if compat.PY35:
+        __await__ = __iter__
+
+    def result(self):
+        return self.con_future.result()
 
 class unsync(object):
     loop = asyncio.new_event_loop()
@@ -17,7 +46,8 @@ class unsync(object):
         self.f = args[0]
 
     def __call__(self, *args, **kwargs):
-        return asyncio.run_coroutine_threadsafe(self.f(*args, **kwargs), self.loop)
+        #return asyncio.ensure_future(self.f(*args, **kwargs), loop=self.loop)
+        return Unfuture(self.f(*args, **kwargs), self.loop)
 
 
 unsync.thread = Thread(target=unsync.thread_target, args=(unsync.loop,), daemon=True)
