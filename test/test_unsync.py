38795082ff4f7375d14c61849dd155c8abc88b7b
Alex Sherman <asherman1024@gmail.com>
2019-02-26 01:57:21 -0600

    Add another test for initiating coroutines from the main thread

--- test/test_unsync.py
+++ test/test_unsync.py
@@ -107,3 +107,13 @@ class DecoratorTests(TestCase):
             return await wait()
 
         self.assertEqual('faff', unsync_func().result())
+
+    def test_regular_main_thread_async(self):
+        async def wait():
+            await asyncio.sleep(0.1)
+            return 'faff'
+        @unsync
+        async def unsync_func(future):
+            return await future
+
+        self.assertEqual('faff', unsync_func(wait()).result())
