a52a0b04980dcaf6dc2fd734aa9d7be9d8960bbe
Alex Sherman <asherman1024@gmail.com>
2019-04-29 12:52:35 -0500

    Version 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='unsync',
-    version='1.1',
+    version='1.2',
     packages=['unsync'],
     url='https://github.com/alex-sherman/unsync',
     license='MIT',
