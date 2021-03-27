3fd415500dbeb87ed96347dcbc05bc2261cc859a
Alex Sherman <asherman1024@gmail.com>
2021-03-27 17:23:06 -0500

    Version 1.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='unsync',
-    version='1.3.1',
+    version='1.3.2',
     packages=['unsync'],
     url='https://github.com/alex-sherman/unsync',
     license='MIT',
